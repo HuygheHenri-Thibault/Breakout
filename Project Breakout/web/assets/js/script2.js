a831c118015badb95fcf6f1f7dc517967b3a3db1
VerriestFredrik <fredrik.verriest@student.howest.be>
2017-10-14 16:21:34 +0200

    Added javascript in second js file
    Testing paths on the map with layers

---
+++ Project Breakout/web/assets/js/script2.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Fredr on 10/2/2017.
+ */
+
+$(document).ready(function () {
+
+    $("#PathToVillage").hide();
+    $("#PathToLonelyRock").hide();
+    $("#PathToExplodingMountain").hide();
+    $("#PathToSignPost").hide();
+    $("#PathToDragonsDen").hide();
+    $("#PathToSpire").hide();
+    $("#PathToTheBlueKeep").hide();
+});
+
+
+
+
+/*
+    Layer 3 --> PathToVillage
+    layer 4 --> PathToLonelyRock
+    layer 6 --> PathToExplodingMountain
+    layer 7 --> PathToSignPost
+    layer5 --> PathToDragonsDen
+    layer8 --> PathToSpire
+    layer9 --> PathToTheBlueKeep*/
