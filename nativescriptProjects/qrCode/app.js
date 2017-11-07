8f340a6d90a1027d50fd35db41526d3c15712ded
VerriestFredrik <fredrik.verriest@student.howest.be>
2017-11-07 14:30:13 +0100

    adjusted the code to work and recognize socket.io dependency

--- nativescriptProjects/qrCode/app.js
+++ nativescriptProjects/qrCode/app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const app = express(); // instance of express
-const port =3000;
-const path = require('path')
-var server = require('http').createServer(app);
+var express = require('express')
+  , http = require('http');
+var app = express();
+var path = require('path');
+var port = 3000;
+var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-
+server.listen(port)
 app.use(express.static('public'));
 app.use(express.static('src/views', {index: 'qrcode.html'}))
 app.use(express.static('src/views'));
 app.use(express.static('js'));
-app.listen(port, function(){
-  console.log('callback called, server started on 3000')
-});
+
 app.get('/books', function(req,res){
   res.send('Hello books ')
 });
 console.log(__dirname);
+
+io.on('connection', function(){
+  console.log('User connected');
+});
 /*app.get('*',function (req, res) {
         res.redirect('src/views');
 });*/
