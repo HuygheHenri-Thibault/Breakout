c451cce00009b8ef45a2974a587a1a8c7523a5ef
VerriestFredrik <fredrik.verriest@student.howest.be>
2017-11-07 12:58:06 +0100

    Added default route for index file
    Qrcode is included

--- nativescriptProjects/qrCode/app.js
+++ nativescriptProjects/qrCode/app.js
@@ -4,16 +4,19 @@ const port =3000;
 const path = require('path')
 
 app.use(express.static('public'));
+app.use(express.static('src/views', {index: 'qrcode.html'}))
 app.use(express.static('src/views'));
 app.use(express.static('js'));
-app.get('/', function(req,res){
+/*app.get('/', function(req,res){
   res.send('Hello world ')
 
-});
+});*/
 app.get('/books', function(req,res){
   res.send('Hello books ')
 });
-
+app.get('*',function (req, res) {
+        res.redirect('src/views');
+});
 console.log(__dirname);
 app.listen(port, function(){
   console.log('callback called, server started on 3000')
