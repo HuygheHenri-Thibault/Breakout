efc243c464c6b41a87f2df9f04455fbc3f3b8e7e
VerriestFredrik <fredrik.verriest@student.howest.be>
2017-11-07 13:11:29 +0100

    Added server and socket.io listener +dependencies

--- nativescriptProjects/qrCode/app.js
+++ nativescriptProjects/qrCode/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express(); // instance of express
 const port =3000;
 const path = require('path')
-
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 app.use(express.static('public'));
 app.use(express.static('src/views', {index: 'qrcode.html'}))
 app.use(express.static('src/views'));
 app.use(express.static('js'));
+app.listen(port, function(){
+  console.log('callback called, server started on 3000')
+});
 /*app.get('/', function(req,res){
   res.send('Hello world ')
 
@@ -14,10 +18,7 @@ app.use(express.static('js'));
 app.get('/books', function(req,res){
   res.send('Hello books ')
 });
-app.get('*',function (req, res) {
+/*app.get('*',function (req, res) {
         res.redirect('src/views');
-});
+});*/
 console.log(__dirname);
-app.listen(port, function(){
-  console.log('callback called, server started on 3000')
-});
