604e9694863a41d46fecfd3c2c6ce2cf57c49fbd
VerriestFredrik <fredrik.verriest@student.howest.be>
2017-11-07 14:10:26 +0100

    added client side socket.io connection

--- nativescriptProjects/qrCode/app.js
+++ nativescriptProjects/qrCode/app.js
@@ -4,6 +4,7 @@ const port =3000;
 const path = require('path')
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
 app.use(express.static('public'));
 app.use(express.static('src/views', {index: 'qrcode.html'}))
 app.use(express.static('src/views'));
@@ -11,14 +12,14 @@ app.use(express.static('js'));
 app.listen(port, function(){
   console.log('callback called, server started on 3000')
 });
+app.get('/books', function(req,res){
+  res.send('Hello books ')
+});
+console.log(__dirname);
+/*app.get('*',function (req, res) {
+        res.redirect('src/views');
+});*/
 /*app.get('/', function(req,res){
   res.send('Hello world ')
 
 });*/
-app.get('/books', function(req,res){
-  res.send('Hello books ')
-});
-/*app.get('*',function (req, res) {
-        res.redirect('src/views');
-});*/
-console.log(__dirname);
