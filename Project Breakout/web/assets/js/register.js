881df6bfee617304902f522ca4af7d8b132bee27
Henri-Thibault Huyghe <Henri-Thibault.Huyghe@student.howest.be>
2017-11-13 08:28:36 +0100

    took out todo's

--- Project Breakout/web/assets/js/register.js
+++ Project Breakout/web/assets/js/register.js
@@ -14,7 +14,4 @@ function passwordsMatch(){
 $(document).ready(function() {
   console.log("register.js is loaded");
   $("#passwordCheck").on("change", checkIfPasswordsMatch);
-  // document.documentElement.style.setProperty(`--accent-color`, 'red');
-  // TODO: Nice idea for accent color change but needs to be done another way
-  // Maybe with add & remove class?
 });
