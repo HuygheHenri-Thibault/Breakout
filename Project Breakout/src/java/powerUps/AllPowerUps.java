f11b7e5e14330fec21c0f4417cac8037d9c372f3
Faunus01 <michael.coudeville@gmail.com>
2017-12-08 09:39:03 +0100

    klasse aangemaakt zodat de bricks random powerups of powerdowns krijgen

--- Project Breakout/src/java/powerUps/AllPowerUps.java
+++ Project Breakout/src/java/powerUps/AllPowerUps.java
@@ -5,15 +5,59 @@
  */
 package powerUps;
 
+import domain.Game;
+import domain.Level;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author micha
  */
-public enum AllPowerUps {
+public class AllPowerUps {
     //alle power ups van de database halen
-    //van alle data, variabelen maken.
+    //van alle data, variabelen maken. 
     
+    List<PowerUpOrDown> powerUps = new ArrayList<>();
+    List<PowerUpOrDown> powerDowns = new ArrayList<>();
     
+    public void addToPowerUpList(PowerUpOrDown powerup){
+        powerUps.add(powerup);
+    }
+    
+    public void addToPowerDownList(PowerUpOrDown powerup){
+        powerDowns.add(powerup);
+    }
+    
+    public List<PowerUpOrDown> getAllPowerUpsAndDowns(){
+        List<PowerUpOrDown> allPowerUps = new ArrayList<>();
+        allPowerUps.addAll(powerUps);
+        allPowerUps.addAll(powerDowns);
+        return allPowerUps;
+    }
+    
+    public PowerUpOrDown getRandomPowerUpOrDown(Level thisLevel, List<PowerUpOrDown> powerUpsAlreadyInLevel){
+        Random r = new Random();
+        int randomNumber = r.nextInt(powerUpsAlreadyInLevel.size() - 1 + 1) + 1;
+        while(powerUpsAlreadyInLevel.contains(getAllPowerUpsAndDowns().get(randomNumber))){
+            randomNumber =  r.nextInt(powerUpsAlreadyInLevel.size() - 1 + 1) + 1;
+        }
+        int powerUpOrDownsChance = r.nextInt(100 - 0 + 0) + 0;
+        if(powerUpOrDownsChance < 50 * thisLevel.getRatios().get(2).getRatio()){
+            return getRandomPowerUp(randomNumber);
+        } else {
+            return getRandomPowerDown(randomNumber);
+        }        
+    }
+    
+    private PowerUpOrDown getRandomPowerUp(int randomNumber){
+        return powerUps.get(randomNumber);
+    }
+    
+    private PowerUpOrDown getRandomPowerDown(int randomNumber){
+        return powerDowns.get(randomNumber);
+    }
 //    enum AllBonuses{
 //    Bonus1(BonusClass1::new),
 //    // ...
