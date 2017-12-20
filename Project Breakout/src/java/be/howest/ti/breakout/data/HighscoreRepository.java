8355114ed61da9869175622fc2ec4098d228819a
BrechtWillemsTI <brecht.willems2@student.howest.be>
2017-12-20 08:31:50 +0100

    update of MysqlhighscoreClass.

--- Project Breakout/src/java/be/howest/ti/breakout/data/HighscoreRepository.java
+++ Project Breakout/src/java/be/howest/ti/breakout/data/HighscoreRepository.java
@@ -1,22 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package be.howest.ti.breakout.data;
 
-import be.howest.ti.breakout.domain.game.Highscore;
+import be.howest.ti.breakout.domain.game.MultiPlayerHighscore;
 import be.howest.ti.breakout.domain.game.SinglePlayerHighscore;
 import be.howest.ti.breakout.domain.game.User;
 import java.util.List;
 
-/**
- *
- * @author Henri
- */
 public interface HighscoreRepository {
     public List<SinglePlayerHighscore> getAllSingleplayerHighscores();
     public SinglePlayerHighscore getUserHighscore(User u);
     public void updateSinglePlayerHighscore(SinglePlayerHighscore sph);
-    public void removeHighscore(Highscore h);
+    public List<MultiPlayerHighscore> getAllMultiplayerScores();
+    public int insertScoreIntoMultiplayerScores(int totalscore);
+    public void insertPlayerScoresForMultiplayer(User u, int multiplayerID, int score);
 }
--- Project Breakout/src/java/be/howest/ti/breakout/data/MySQLHighscoreRepository.java
+++ Project Breakout/src/java/be/howest/ti/breakout/data/MySQLHighscoreRepository.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package be.howest.ti.breakout.data;
 
 import be.howest.ti.breakout.data.util.MySQLConnection;
-import be.howest.ti.breakout.domain.game.Highscore;
+import be.howest.ti.breakout.domain.game.MultiPlayerHighscore;
 import be.howest.ti.breakout.domain.game.SinglePlayerHighscore;
 import be.howest.ti.breakout.domain.game.User;
 import java.sql.Connection;
@@ -16,6 +12,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import be.howest.ti.breakout.util.BreakoutException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -27,8 +26,12 @@ public class MySQLHighscoreRepository implements HighscoreRepository {
     
     private static final String GET_ALL_SINGLEPLAYERHIGHSCORES = "SELECT username, spHighscore FORM breakout.user";
     private static final String GET_USERS_SINGLEPLAYERHIGHSCORES = "SELECT username, spHighscore FROM breakout.userhighscore WHERE user_id = ?";
-    private static final String UPDATE_SINGLEPLAYERHIGHSCORE = "UPDATE breakout.user SET spHighscore = ? WHERE id = ?";
+    private static final String UPDATE_SINGLEPLAYERHIGHSCORE = "UPDATE breakout.user SET spHighscore = ? WHERE username = ?";
     private static final String DELETE_HIGHSCORE = "DELETE * FROM breakout.userhighscore WHERE user_id = ? AND highscore = ?";
+    private static final String GET_ALL_MULTIPLAYER_SCORES = "select * from multiplayerhighscore";
+    private static final String GET_ALL_PLAYERS_FROM_MULTIPLAYER = "select * from multiplayerscores where multiplayerID = ?";
+    private static final String INSERT_MULTIPLAYER_SCORE = "INSERT INTO multiplayerhighscore(totalScore) values(?)";
+    private static final String INSERT_PLAYER_SCORE_FROM_MULTIPLAYER = "INSERT INTO multiplayerscores(userID, multiplayerID, score values(?, ?, ?)";
     
     @Override
     public List<SinglePlayerHighscore> getAllSingleplayerHighscores() {
@@ -72,7 +75,7 @@ public class MySQLHighscoreRepository implements HighscoreRepository {
     }
 
     @Override
-    public void addSinglePlayerHighscore(SinglePlayerHighscore sph) {
+    public void updateSinglePlayerHighscore(SinglePlayerHighscore sph) {
         if (sph.getScore() > Repositories.getUserRepository().getUserWithUsername(sph.getUser().getUsername()).getSinglePlayerScore()){
             try(Connection con = MySQLConnection.getConnection();
                 PreparedStatement stmt = con.prepareStatement(UPDATE_SINGLEPLAYERHIGHSCORE)) {
@@ -82,22 +85,77 @@ public class MySQLHighscoreRepository implements HighscoreRepository {
                 stmt.executeUpdate();            
 
             } catch(SQLException ex) {
-                throw new BreakoutException("Couldn't add the singleplayer highscore", ex);
+                throw new BreakoutException("Couldn't update the singleplayer highscore", ex);
             }
         }
     }
 
     @Override
-    public void removeHighscore(Highscore h) {
+    public List<MultiPlayerHighscore> getAllMultiplayerScores() {
         try(Connection con = MySQLConnection.getConnection();
-            PreparedStatement stmt = con.prepareStatement(DELETE_HIGHSCORE)) {
-            
-            stmt.setInt(1, h.getUserWithHighscore().getUserId());
-            stmt.setInt(2, h.getHighscore());
-            stmt.executeUpdate();            
-            
+            PreparedStatement stmt = con.prepareStatement(GET_ALL_MULTIPLAYER_SCORES)) {
+            try(ResultSet rs = stmt.executeQuery()) {
+                List<MultiPlayerHighscore> mpScores = new ArrayList<>();
+                while(rs.next()) {
+                    int id = rs.getInt("id");
+                    SinglePlayerHighscore sph = this.getIndividualScore(id);
+                    Map<User, Integer> playerList = new HashMap<>();
+                    playerList.put(sph.getUser(), sph.getScore());
+                    MultiPlayerHighscore mph = new MultiPlayerHighscore(id, playerList);
+                    mpScores.add(mph);
+                }
+                return mpScores;
+            }
         } catch(SQLException ex) {
-            throw new BreakoutException("Couldn't delete that user", ex);
+            throw new BreakoutException("Couldn't get all multi player highscores", ex);
+        }
+    }
+    
+    private SinglePlayerHighscore getIndividualScore(int id){
+        try(Connection con = MySQLConnection.getConnection();
+            PreparedStatement stmt = con.prepareStatement(GET_ALL_PLAYERS_FROM_MULTIPLAYER)) {
+            stmt.setInt(1, id);
+            try(ResultSet rs = stmt.executeQuery()) {
+                SinglePlayerHighscore sph = null;
+                while(rs.next()) {
+                    User player = Repositories.getUserRepository().getUserWithId(rs.getInt("userID"));
+                    int individualScore = rs.getInt("score");
+                    sph = new SinglePlayerHighscore(player, individualScore);
+                }
+                return sph;
+            }  
+        } catch(SQLException ex) {
+            throw new BreakoutException("Couldn't get all the individual scores from multiplayer", ex);
+        }
+    }
+
+    @Override
+    public int insertScoreIntoMultiplayerScores(int totalscore) {
+        try(Connection con = MySQLConnection.getConnection();
+            PreparedStatement stmt = con.prepareStatement(INSERT_MULTIPLAYER_SCORE, Statement.RETURN_GENERATED_KEYS)) {
+                stmt.setInt(1, totalscore);
+                stmt.executeUpdate();
+                ResultSet rs = stmt.getGeneratedKeys();
+                int generatedKey = 0;
+                if (rs.next()) {
+                    generatedKey = rs.getInt(1);
+                }
+                return generatedKey;
+            } catch(SQLException ex) {
+                throw new BreakoutException("Couldn't add the totalscore", ex);
+        }
+    }
+
+    @Override
+    public void insertPlayerScoresForMultiplayer(User u, int multiplayerID, int score) {
+        try(Connection con = MySQLConnection.getConnection();
+            PreparedStatement stmt = con.prepareStatement(INSERT_PLAYER_SCORE_FROM_MULTIPLAYER)) {
+                stmt.setInt(1, u.getUserId());
+                stmt.setInt(2, multiplayerID);
+                stmt.setInt(3, score);
+                stmt.executeUpdate();
+        } catch(SQLException ex) {
+            throw new BreakoutException("Couldn't add the player scores", ex);
         }
     }
 }
